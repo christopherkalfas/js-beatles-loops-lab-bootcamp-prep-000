f8a673748893ae3fdd1babeb5a0dd0bd5a324eea
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-31 18:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,11 @@
 function theBeatlesPlay(musicians, instruments){
   var theBeatles=[];
   
-  
-  for (let i=0; i<1; i++){
- 
-
-
- 
- theBeatles.push(`${musicians[0]} plays ${instruments[0]}`);
-theBeatles.push(`${musicians[1]} plays ${instruments[1]}`);
-theBeatles.push(`${musicians[2]} plays ${instruments[2]}`);
-theBeatles.push(`${musicians[3]} plays ${instruments[3]}`);
+    for (let i=0; i<1; i++){
+      theBeatles.push(`${musicians[0]} plays ${instruments[0]}`);
+      theBeatles.push(`${musicians[1]} plays ${instruments[1]}`);
+      theBeatles.push(`${musicians[2]} plays ${instruments[2]}`);
+      theBeatles.push(`${musicians[3]} plays ${instruments[3]}`);
 }
 return theBeatles;
 
